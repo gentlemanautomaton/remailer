c781763fa76b123bf595dd399352bb8f31f2a39b
Dusty Wilson <dusty@linux.com>
2017-09-24 11:03:12 +0000

    Debug ENV in Docker Hub

--- cmd/server/build.sh
+++ cmd/server/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+env > /tmp/ENV
 go get -v github.com/dustywilson/remailer github.com/flashmob/go-guerrilla
 go install -ldflags "-X main.commitVersion=$SOURCE_COMMIT" github.com/dustywilson/remailer/cmd/server
