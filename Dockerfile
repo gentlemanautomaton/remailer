764a51dca485113c28edb4ba93a98db67419cadd
Dusty Wilson <dusty@linux.com>
2017-09-24 11:00:35 +0000

    Dockerfile adjustments

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,7 @@ EXPOSE 25
 
 WORKDIR /go/src/github.com/dustywilson/remailer
 COPY . .
-
-WORKDIR /go/src/github.com/dustywilson/remailer/cmd/server
-RUN bash build.sh
+RUN bash cmd/server/build.sh
 
 WORKDIR /config
 CMD ["/go/bin/server"]
--- cmd/server/build.sh
+++ cmd/server/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 go get -v github.com/dustywilson/remailer github.com/flashmob/go-guerrilla
-go install -ldflags "-X main.commitVersion=$(git log | head -n1 | cut -d' ' -f2)" github.com/dustywilson/remailer/cmd/server
+go install -ldflags "-X main.commitVersion=$SOURCE_COMMIT" github.com/dustywilson/remailer/cmd/server
