694a47bd37c8d720676abf8be9ad923a6b8ab7fe
Dusty Wilson <dusty@linux.com>
2017-09-24 12:33:30 +0000

    Progress on HTTPS posting

--- post.go
+++ post.go
@@ -1,13 +1,22 @@
 package remailer
 
 import (
+	"errors"
+	"net/http"
 	"net/url"
 
 	"github.com/flashmob/go-guerrilla/backends"
 	"github.com/flashmob/go-guerrilla/mail"
+	"github.com/flashmob/go-guerrilla/response"
 )
 
 func (r *remailer) postMessage(u url.URL, e *mail.Envelope) (backends.Result, error) {
-	//e.String()
-	return nil, nil
+	res, err := http.Post(u.String(), "text/plain", e.NewReader())
+	if err != nil {
+		backends.Log().WithError(backends.StorageNotAvailable).Info("post: " + err.Error())
+		return backends.NewResult(response.Canned.FailReadErrorDataCmd), errors.New("Temporary Server Error, try again shortly")
+	}
+	backends.Log().Info("post: " + res.Status)
+
+	return nil, nil // FIXME
 }
