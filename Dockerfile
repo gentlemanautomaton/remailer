6a10211760dea9cde464d2974be30d45df70633f
Dusty Wilson <dusty@linux.com>
2017-09-24 11:07:19 +0000

    Debug ENV in Docker Hub

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM golang:latest
 
 EXPOSE 25
 
+RUN env > /tmp/ENV
+RUN pwd > /tmp/PWD
+RUN git rev-parse HEAD > /tmp/HEAD
 WORKDIR /go/src/github.com/dustywilson/remailer
 COPY . .
 RUN bash cmd/server/build.sh
--- cmd/server/build.sh
+++ cmd/server/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-env > /tmp/ENV
 go get -v github.com/dustywilson/remailer github.com/flashmob/go-guerrilla
 go install -ldflags "-X main.commitVersion=$SOURCE_COMMIT" github.com/dustywilson/remailer/cmd/server
